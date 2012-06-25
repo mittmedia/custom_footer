70fdb445f1c8e187c2da82d8f4262b12181695bb
Fredrik Sundström <fredrik.sundstrom@gmail.com>
2012-06-25 14:39:59 +0200

    Cleaned house

---
+++ custom_footer.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: Custom footer
+Plugin URI: https://github.com/mittmedia/portal
+Description: It's a blog portal.
+Version: 1.0.0
+Author: Fredrik Sundström
+Author URI: https://github.com/fredriksundstrom
+License: MIT
+*/
+
+/*
+Copyright (c) 2012 Fredrik Sundström
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+require_once( 'wp_mvc-1.0.0/init.php' );
+
+$custom_footer_app = new \WpMvc\Application();
+
+$custom_footer_app->init( 'CustomFooter', WP_PLUGIN_DIR . '/custom_footer' );
+
+// WP: Add pages
+add_action( "admin_menu", "custom_footer" );
+function custom_footer()
+{
+  add_options_page( "Custom footer", "Custom footer", "Administrator", "custom_footer_menu", "custom_footer_page" );
+}
+
+function custom_footer_page()
+{
+  global $custom_footer_app;
+  
+  $custom_footer_app->footer_controller->index();
+}
